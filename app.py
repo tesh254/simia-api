94dfc39c918c2a81b6a88f00d54cbb7c2ffba31b
wachira_Dev <ewachira254@gmail.com>
2020-09-11 16:34:57 +0300

    ⚙️ add dynamic app environment configuration

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 
 @app.route("/")
 def hello():
-    return "Hello world"
+    return os.environ['APP_SETTINGS']
 
 
 if __name__ == "__main__":
